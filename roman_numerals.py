aaad04cd5926cd76e13c6cb6375f799304ea037e
Sue Yang <sueyang112@hotmail.com>
2022-09-27 16:10:13 -0400

    Followed along with youtube

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,16 +1,18 @@
 class Solution:
+    # This calls the function and converts it into a interger
     def romanToInt(self, s: str) -> int:
 
+        # This is providing a dictonary of what each value equals to
         roman = {"I": 1, "V": 5, "X": 10, "L": 50,
                  "C": 100, "D": 500, "M": 1000}
 
-        res = 0
+        ans = 0  # This starts the anwser at 0
 
+        # This cycles the i through the entire length of the roman numeral given
         for i in range(len(s)):
-            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
-                res -= roman[s[i]]
-
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+                ans -= roman[s[i]]
             else:
-                res += roman[s[i]]
+                ans += roman[s[i]]
 
-        return res
+        return ans
